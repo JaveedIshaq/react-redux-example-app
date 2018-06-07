2fe311b455d1e8df673a45a220ceda03fb9ae3f2
Javeed Ishaq <ishaqjaveed1@gmail.com>
2018-06-07 09:21:26 +0500

    from ubuntu

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to React Redux App By Javeed Ishaq</h1>
         </header>
         <Postform />
         <hr />
